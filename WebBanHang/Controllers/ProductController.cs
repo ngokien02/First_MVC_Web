cc8c608545bc6d0f592b898e5520232cf2ec6ee1
ngokien02 <140368220+ngokien02@users.noreply.github.com>
2025-05-19 23:42:28 +0700

    qlbanhang

--- WebBanHang/Controllers/ProductController.cs
+++ WebBanHang/Controllers/ProductController.cs
@@ -6,63 +6,162 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace WebBanHang.Controllers
 {
-    public class ProductController : Controller
-    {
-        private readonly ApplicationDbContext _db;
-        private readonly IWebHostEnvironment _hosting;
-        public ProductController(ApplicationDbContext db, IWebHostEnvironment hosting)
-        {
-            _db = db;
-            _hosting = hosting;
-        }
-        public IActionResult Index()
-        {
-            var productList = _db.Products.Include(x => x.Category).ToList();
-            return View(productList);
-        }
-        public IActionResult Add()
-        {
-            ViewBag.CategoryList = _db.Categories.Select(x => new SelectListItem
-            {
-                Value = x.Id.ToString(),
-                Text = x.Name
-            });
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Add(Product p, IFormFile ImageUrl)
-        {
-            if (ModelState.IsValid)
-            {
-                if (ImageUrl != null)
-                {
-                    p.ImageUrl = SaveImage(ImageUrl);
-                }
-
-                _db.Products.Add(p);
-                _db.SaveChanges();
-                TempData["success"] = "Thêm sản phẩm thành công";
-
-                return RedirectToAction("Index");
-
-            }
+	public class ProductController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		private readonly IWebHostEnvironment _hosting;
+		public ProductController(ApplicationDbContext db, IWebHostEnvironment hosting)
+		{
+			_db = db;
+			_hosting = hosting;
+		}
+		public IActionResult Index(int page = 1)
+		{
+			int pageSize = 5;
+			var totalProducts = _db.Products.Count();
+			var productList = _db.Products
+				.Include(x => x.Category)
+				.OrderByDescending(x => x.Id)
+				.Skip((page - 1) * pageSize)
+				.Take(pageSize)
+				.ToList();
+			ViewBag.TotalPages = (int)Math.Ceiling((double)totalProducts / pageSize);
+			ViewBag.CurrentPage = page;
+			return View(productList);
+		}
+		public IActionResult Add()
+		{
 			ViewBag.CategoryList = _db.Categories.Select(x => new SelectListItem
 			{
 				Value = x.Id.ToString(),
 				Text = x.Name
 			});
 			return View();
-        }
-        private string SaveImage(IFormFile img)
-        {
-            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(img.FileName);
-            var path = Path.Combine(_hosting.WebRootPath, @"images/products");
-            var saveFile = Path.Combine(path, fileName);
+		}
+		[HttpPost]
+		public IActionResult Add(Product p, IFormFile ImageUrl)
+		{
+			if (ModelState.IsValid)
+			{
+				if (ImageUrl != null)
+				{
+					p.ImageUrl = SaveImage(ImageUrl);
+				}
+
+				_db.Products.Add(p);
+				_db.SaveChanges();
+				TempData["success"] = "Thêm sản phẩm thành công";
+
+				return RedirectToAction("Index");
+
+			}
+			ViewBag.CategoryList = _db.Categories.Select(x => new SelectListItem
+			{
+				Value = x.Id.ToString(),
+				Text = x.Name
+			});
+			return View();
+		}
+		private string SaveImage(IFormFile img)
+		{
+			var fileName = Guid.NewGuid().ToString() + Path.GetExtension(img.FileName);
+			var path = Path.Combine(_hosting.WebRootPath, @"images/products");
+			var saveFile = Path.Combine(path, fileName);
 			using (var filestream = new FileStream(saveFile, FileMode.Create))
 			{
 				img.CopyTo(filestream);
 			}
 			return @"images/products/" + fileName;
 		}
-    }
+		public IActionResult Update(int id)
+		{
+			var product = _db.Products.Find(id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+			//truyền danh sách thể loại cho View để sinh ra điều khiển DropDownList
+			ViewBag.CategoryList = _db.Categories.Select(x => new SelectListItem
+			{
+
+				Value = x.Id.ToString(),
+				Text = x.Name
+			});
+			return View(product);
+		}
+		[HttpPost, ActionName("Update")]
+		public IActionResult Update(Product product, IFormFile ImageUrl)
+		{
+			if (ModelState.IsValid) //kiem tra hop le
+			{
+				var existingProduct = _db.Products.Find(product.Id);
+				if (ImageUrl != null)
+				{
+					//xu ly upload và lưu ảnh đại diện mới
+					product.ImageUrl = SaveImage(ImageUrl);
+					//xóa ảnh cũ (nếu có)
+					if (!string.IsNullOrEmpty(existingProduct.ImageUrl))
+					{
+						var oldFilePath = Path.Combine(_hosting.WebRootPath, existingProduct.ImageUrl);
+						if (System.IO.File.Exists(oldFilePath))
+						{
+							System.IO.File.Delete(oldFilePath);
+						}
+					}
+				}
+				else
+				{
+					product.ImageUrl = existingProduct.ImageUrl;
+				}
+				//cập nhật product vào table Product
+				existingProduct.Name = product.Name;
+				existingProduct.Description = product.Description;
+				existingProduct.Price = product.Price;
+				existingProduct.CategoryId = product.CategoryId;
+				existingProduct.ImageUrl = product.ImageUrl;
+				_db.SaveChanges();
+				TempData["success"] = "Cập nhật sản phẩm thành công";
+				return RedirectToAction("Index");
+			}
+			ViewBag.CategoryList = _db.Categories.Select(x => new SelectListItem
+			{
+				Value = x.Id.ToString(),
+				Text = x.Name
+			});
+			return View("index");
+		}
+		public IActionResult Delete(int id)
+		{
+			var product = _db.Products.Find(id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+			return View(product);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeleteConfirmed(int id)
+		{
+			var product = _db.Products.Find(id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+			// xoá hình cũ của sản phẩm
+			if (!String.IsNullOrEmpty(product.ImageUrl))
+			{
+				var oldFilePath = Path.Combine(_hosting.WebRootPath, product.ImageUrl);
+				if (System.IO.File.Exists(oldFilePath))
+				{
+					System.IO.File.Delete(oldFilePath);
+				}
+			}
+			// xoa san pham khoi CSDL
+			_db.Products.Remove(product);
+			_db.SaveChanges();
+			//chuyen den action index
+			TempData["DeleteSuccess"] = "Xóa sản phẩm thành công!";
+			return RedirectToAction("Index");
+		}
+	}
 }
